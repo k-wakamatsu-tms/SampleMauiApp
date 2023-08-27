7caed8752fe5d62e51cd7cc514b446dd15ead90b
wakamatsu_kouta <kw0205.sora@gmail.com>
2023-08-28 03:09:33 +0900

    プロジェクト ファイルを追加します。

---
+++ SampleMauiApp/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using SampleMauiApp.ViewModels;
+
+namespace SampleMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MonkeysViewModel();
+        }
+
+    }
+}
