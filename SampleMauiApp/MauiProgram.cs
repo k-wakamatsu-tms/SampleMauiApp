9f35586699b8e30bf10dc62d4931095ba6fe2618
wakamatsu_kouta <kw0205.sora@gmail.com>
2023-09-11 01:39:51 +0900

    aa

--- SampleMauiApp/MauiProgram.cs
+++ SampleMauiApp/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace SampleMauiApp
 {
@@ -15,13 +16,34 @@ namespace SampleMauiApp
                     //fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                     fonts.AddFont("FiraSans-Light.ttf", "RegularFont");
                     fonts.AddFont("FiraSans-Medium.ttf", "MediumFont");
+                })
+                .ConfigureLifecycleEvents(events =>
+                {
+#if ANDROID
+                    events.AddAndroid(android => android.OnCreate((activity, bundle) => MakeStatusTrancelucent(activity)));
+                    static void MakeStatusTrancelucent(Android.App.Activity activity)
+                    {
+                        activity.Window.SetFlags(Android.Views.WindowManagerFlags.LayoutNoLimits, Android.Views.WindowManagerFlags.LayoutNoLimits);
+
+                        activity.Window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
+                        activity.Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+                    }
+#endif
                 });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
+            RegisterAppServices(builder.Services);
+
             return builder.Build();
+        }
+
+        private static void RegisterAppServices(IServiceCollection services)
+        {
+
+            services.AddSingleton<IConnectivity>(Connectivity.Current);
         }
     }
 }
